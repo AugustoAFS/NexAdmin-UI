7484b7cb347ee627fa413d629821547dc279bb20
Augusto Farias dos Santos <augusto.farias.2003@gmail.com>
2025-06-04 10:42:55 -0300

    Ajustando para nao dar copy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { CurriculosComponent } from './pages/curriculos/curriculos.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +11,6 @@ export const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'home', component: HomeComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'curriculos', component: CurriculosComponent },
       { path: '**', redirectTo: '', pathMatch: 'full' }
     ]
   }
